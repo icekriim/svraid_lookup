b07746c59de32ced6fbb3c0115b408cb5a36927b
icekriim <icekriim11@gmail.com>
2023-04-06 01:24:58 +0800

    Update side_panel.rs

--- src/side_panel.rs
+++ src/side_panel.rs
@@ -36,42 +36,7 @@ pub fn draw_side_panel(app: &mut SVRaidLookup, ctx: &Context) {
         });
         ui.add_space(15.0);
         ui.vertical_centered_justified(|ui| {
-            if ui.button("Load Latest Event Data").clicked() {
-                let request = ehttp::Request::get("https://citrusbolt.net/bcat/v/latest/raid/files/raid_enemy_array");
-                let clone = app.event_encounters.clone();
-                ehttp::fetch(request, move |response| {
-                    if let Ok(response) = response {
-                        if let Ok(raid_table_array) = sv_raid_reader::delivery_enemy_table_generated::root_as_delivery_raid_enemy_table_array(&response.bytes) {
-                            if let Ok(mut event_encounters) = clone.lock() {
-                                *event_encounters = raid_table_array.values().into_iter().map(|t| t.raidEnemyInfo().into()).collect::<Vec<_>>();
-                            }
-                        }
-                    }
-                });
-
-                let request = ehttp::Request::get("https://citrusbolt.net/bcat/v/latest/raid/files/fixed_reward_item_array");
-                let clone = app.fixed_event_item.clone();
-                ehttp::fetch(request, move |response| {
-                    if let Ok(response) = response {
-                        if let Ok(fixed_item_table) = sv_raid_reader::raid_fixed_reward_item_generated::root_as_raid_fixed_reward_item_array(&response.bytes) {
-                            if let Ok(mut fixed_event_items) = clone.lock() {
-                                *fixed_event_items = fixed_item_table.into();
-                            }
-                        }
-                    }
-                });
-
-                let request = ehttp::Request::get("https://citrusbolt.net/bcat/v/latest/raid/files/lottery_reward_item_array");
-                let clone = app.lottery_event_items.clone();
-                ehttp::fetch(request, move |response| {
-                    if let Ok(response) = response {
-                        if let Ok(lottery_item_table) = sv_raid_reader::raid_lottery_reward_item_generated::root_as_raid_lottery_reward_item_array(&response.bytes) {
-                            if let Ok(mut lottery_event_items) = clone.lock() {
-                                *lottery_event_items = lottery_item_table.into();
-                            }
-                        }
-                    }
-                });
+            if ui.button("Item Farming Raid").clicked() {
             }
         });
 
